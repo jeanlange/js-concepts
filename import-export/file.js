d39ac5520cfbaad2bdc63f12faa92b2241af3876
Jean Lange <jean.f.lange@gmail.com>
2024-09-18 15:26:23 -0400

    you can import a lot at once with *

--- import-export/file.js
+++ import-export/file.js
@@ -1,5 +1,4 @@
-import foo from "./library.js";
-import { sayHi as hi } from "./library.js";
+// import everything from this file that was exported, and store it under the name 'lib'
+import * as lib from "./library.js";
 
-console.log(foo);
-hi();
+console.log(lib.personName);
--- import-export/library.js
+++ import-export/library.js
@@ -1,7 +1,3 @@
-// you can export a variable that is just a string!
-const personName = "Teddy";
+export const personName = "Teddy";
 
-export default personName;
-
-// you can export more complicated variables, like functions!
 export const sayHi = () => console.log('hi');
