e1981799d1f9358068a851be9bbff13dfdd63df4
Jean Lange <jean.f.lange@gmail.com>
2024-09-18 15:19:16 -0400

    you can pull in a default export with a different name

--- import-export/file.js
+++ import-export/file.js
@@ -1,9 +1,4 @@
-//const personName = "Jean";
-
-// tell it to bring in 'personName', the variable, from the library file
 // you can import more than one exported thing at once
-import { personName, sayHi } from "./library.js";
-//import { sayHi } from "./library.js";
+import foo from "./library.js";
 
-console.log(personName);
-sayHi();
+console.log(foo);
--- import-export/library.js
+++ import-export/library.js
@@ -1,5 +1,7 @@
 // you can export a variable that is just a string!
-export const personName = "Teddy";
+const personName = "Teddy";
+
+export default personName;
 
 // you can export more complicated variables, like functions!
 export const sayHi = () => console.log('hi');
